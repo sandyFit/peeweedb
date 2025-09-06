b2fe7588754338670da44731e3cb7c62dce60fa8
SandyFit <sandy07r@gmail.com>
2025-09-05 23:54:36 -0500

    feat(database): Add data

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import peewee
 from datetime import datetime
 
-database = peewee.MySQLDatabase('pythondb', 
-                     host='localhost', 
-                     port=3306,
-                     user='root',
-                     password='intro123'
-                     )
+database = peewee.MySQLDatabase(
+    'pythondb',
+    host='localhost',
+    port=3306,
+    user='root',
+    password='intro123'
+)
 
 # users
 class User(peewee.Model):
-    username=peewee.CharField(max_length=50, unique=-True, index=True)
-    email=peewee.CharField(max_length=60, null=False)
-    active=peewee.BooleanField(default=False)
-    created_at=peewee.DateTimeField(default=datetime.now)
-    
+    username = peewee.CharField(max_length=50, unique=True, index=True)
+    email = peewee.CharField(max_length=60, null=False)
+    active = peewee.BooleanField(default=False)
+    created_at = peewee.DateTimeField(default=datetime.now)
+
     class Meta:
-        database=database
-        db_table='users'
+        database = database
+        db_table = 'users'
         
+    def __str__(self):
+        return self.username
+
+
 if __name__ == '__main__':
+    if database.table_exists('users'):
+        User.drop_table()
+        
     User.create_table()
+    
+    user1 = User(username='user1', email='user@example.com')
+    user1.save()   # Save to DB
+    print(user1)   # Prints "user1"
